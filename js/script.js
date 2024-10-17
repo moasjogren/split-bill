cbac96b8326d7b88243ca819863e22af76936896
moasjogren <moasjgren@gmail.com>
2024-10-17 12:03:51 +0200

    testar

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form");
+const friends = document.querySelector(".input-friends");
+const sum = document.querySelector(".input-sum");
+const tip = document.querySelector(".input-tip");
+const result = document.querySelector("p");
+const total = document.querySelector(".total-sum");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  result.innerHTML = `Ni är ${friends.value} antal gäster som ska betala ${sum.value} kronor. Ni har valt att dricksa ${tip.value} procent.`;
+  totalSum();
+});
+
+tip.addEventListener("input", function () {
+  document.querySelector(".dricks").innerHTML = tip.value + "%";
+});
+
+function totalSum() {
+  let totalTip = +sum.value * (+tip.value / 100);
+  let totalPrice = +totalTip + +sum.value;
+  let eachPrice = +totalPrice / +friends.value;
+  total.innerHTML = `Den totala summan är ${totalPrice} kronor. Ni ska betala ${eachPrice} kronor var.`;
+  console.log(eachPrice);
+}
+
+// function perPerson(totalSum, friends) {
+//   if (friends <= 2 || typeof friends != "number") {
+//     return;
+//   }
+// }
